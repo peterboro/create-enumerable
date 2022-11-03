19883b8a91ae789bfbfc632ad57721af308ca84f
Peter Njuguna <Ptahbn@gmail.com>
2022-11-03 16:45:07 +0300

    create module myEnumerable

---
+++ myEnumerable.rb
@@ -0,0 +1,22 @@
+module MyEnumerable
+  def all?
+    for i in self
+      return false unless yield(i)
+    end
+    true
+  end
+
+  def any?
+    for i in self
+      return true if yield(i)
+    end
+    false
+  end
+
+  def filter
+    result = []
+    for i in self
+      result << i if yield(i)
+    end
+    result
+  end
